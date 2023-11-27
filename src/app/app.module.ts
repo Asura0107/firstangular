e53ac3306610869b572d4d40d9d8802777ef61ad
Asura0107 <mircoorosio@gmail.com>
2023-11-27 16:01:30 +0100

    U3-W2-D1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Compenent1Component } from './compenent1/compenent1.component';
+import { Component2Component } from './component2/component2.component';
+import { Component3Component } from './component3/component3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Compenent1Component,
+    Component2Component,
+    Component3Component
   ],
   imports: [
     BrowserModule
